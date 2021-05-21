d9a63bf6d1052b1472448a992536e2b4e29646b0
billucoder <harishp0213@gmail.com>
2021-05-20 22:36:49 -0400

    Conditional

--- src/com/harish/java/IfStatementRunner.java
+++ src/com/harish/java/IfStatementRunner.java
@@ -3,19 +3,15 @@ package com.harish.java;
 public class IfStatementRunner {
 
     public static void main(String[] args) {
-	// write your code here
-
-        int i =24;
-        //i is 25
-        //i is 24
-        // i is not 25 or 24
-        if(i==25){
-            System.out.println("i=25");
+        int i =27;
+        if (i==25) {
+            System.out.println("i= 25");
         } else if (i==24){
-            System.out.println("i=24");
-
+            System.out.println("i=24 ");
         } else {
-            System.out.println("i !=25 and i !24");
+            System.out.println("i is neither 25 or 24");
         }
     }
-}
+
+    }
+
--- src/com/harish/java/MenuRunner.java
+++ src/com/harish/java/MenuRunner.java
@@ -1,42 +1,45 @@
 package com.harish.java;
 
 import java.util.Scanner;
-
+10
 public class MenuRunner {
     public static void main(String[] args) {
-        // Scanner(System.in) // user input
-        // type object = new type(argument);
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number1: "); // user output
-        int number1 = scanner.nextInt();
-        System.out.println("The Number you entered is " + number1);
+        System.out.println("Number1: ");
+        int number1= scanner.nextInt();
 
-        int number2 = scanner.nextInt();
-        System.out.println("The Number you enter is" + number2);
+        System.out.println("Number2: ");
+        int number2= scanner.nextInt();
 
-        System.out.println("choice Avaibale are ");
+
+        System.out.println("Choice Available are ");
         System.out.println("1- Add");
-        System.out.println("2- subtract");
-        System.out.println("3- divide");
-        System.out.println("3- multiply");
+        System.out.println("2- Subtract");
+        System.out.println("3-Divide");
+        System.out.println("4-Multiply");
 
-        System.out.println("Enter choice:");
+        System.out.println("Enter Choice");
         int choice = scanner.nextInt();
-        System.out.println("Your choices are");
-        System.out.println("Number1" + number1);
-        System.out.println("Number2" + number2);
-        System.out.println("choice" + choice);
-        if (choice == 1) {
-            System.out.println("Result" + (number1 + number2));
-        } else if (choice == 2) {
-            System.out.println("Result" + (number1 - number2));
-        } else if (choice == 3) {
-            System.out.println("Result" + (number1 / number2));
-        } else if (choice == 4) {
-            System.out.println("Result" + (number1 * number2));
-        } else {
-            System.out.println("Invalid Operation");
-        }
 
+        System.out.println("your choices are ");
+        System.out.println("Number1 " + number1);
+        System.out.println("Number2 " + number2);
+        System.out.println("Choice " + choice);
+
+        performOperationUsingSwitch(number1, number2, choice);
     }
+
+    private static void performOperationUsingSwitch(int number1, int number2, int choice) {
+        switch("Choice") {
+            case 1:  System.out.println("Result" + (number1 + number2)); break;
+            case 2:  System.out.println("Result" + (number1 - number2)); break;
+            case 3: System.out.println("Result" + (number1 / number2)); break;
+            case 4: System.out.println("Result" + (number1 * number2)); break;
+            case 5 : System.out.println("Invalid Operation"); break;
+
+            }
+    }
+
 }
+
